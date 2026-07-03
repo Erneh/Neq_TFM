07c4aa9aea33966ce0f59c9391fdfa6334226f96
Ernest Perez <78276270+Erneh@users.noreply.github.com>
2026-07-03 11:35:14 +0200

    Upgraded graphical stuff

--- Liouville_tests/eff_test.py
+++ Liouville_tests/eff_test.py
@@ -30,7 +30,7 @@ T = 2*np.pi/omega_l
 gamma = 0.025
 # RABI
 omega1 = np.sqrt(3)*2.7*gamma*np.pi/hbar
-omega0 = omega_l
+omega0 = omega_l*0
 # VALLEY SELECTION IN EFFECTIVE MODEL
 nu = -1
 # MODEL SELECTION
@@ -39,6 +39,12 @@ pol = np.pi/2
 # Energy adjustments
 broad = 0.05
 dE = broad / 1.5
+
+a_l = 0.24595
+hbar = 0.6582
+a_cc = a_l/3**0.5
+
+vf  = 3*a_cc*2.7/(2*hbar)
 
 # Time evolution adjustments
 N_periods = 50
@@ -89,47 +95,37 @@ EF_list = np.arange(autV[0] - 20*dE, autV[1] + 20*dE, dE)
 
 # Initialization and definition of variables
 rho0 = np.outer(np.conj(autE[:,0]), autE[:,0])
+rho = np.copy(rho0)
 
-
-
-al_list = np.linspace(0, 2*np.pi, 10s0)
+al_list = np.linspace(0, 2*np.pi, 10)
 km = np.abs(autV[0])/hvf
 
-DOS_list = np.zeros((len(al_list), len(n_vec_measures), len(EF_list)))
-N_list = np.zeros((len(al_list), len(n_vec_measures), len(EF_list)))
-
-for a in range(len(al_list)):
-    meas_count = 0
-    rho = np.copy(rho0)
-    for i in range(len(t_vec)):
-        km = EF_list/hvf
-        k = km[None,:]*np.array([np.cos(al_list[a]),np.sin(al_list[a])])[:,None]
-        U  = expm(-1j*Ham(k, t_vec[i])*dt/hbar)
-        if i == n_vec_measures[meas_count]:
-            # Calculation of wanted results
-            delta_op = np.linalg.inv((EF_list[:,None,None]*np.eye(2)- Ham(k, t_vec[i]))@(EF_list[:,None,None]*np.eye(2) - Ham(k, t_vec[i])) + np.eye(2)*broad**2)
-            DOS_list[a, meas_count] = broad/np.pi*np.trace(delta_op, axis1=1, axis2=2)
-            N_list[a, meas_count] = broad/np.pi*np.trace(delta_op@rho, axis1=1, axis2=2)
-            meas_count += 1
-        # Time evolution of relevant variables
-        rho = U@rho@np.conj(U.swapaxes(1, 2))
+DOS_list = np.zeros((len(n_vec_measures), len(EF_list)))
+N_list = np.zeros((len(n_vec_measures), len(EF_list)))
 
 
+k = np.array([0, 0])
+meas_count = 0
+for i in range(len(t_vec)):
+   
+    U  = expm(-1j*Ham(k, t_vec[i])*dt/hbar)
+    if i == n_vec_measures[meas_count]:
+        delta_op = np.linalg.inv((EF_list[:,None,None]*np.eye(2)- Ham(k, t_vec[i]))@(EF_list[:,None,None]*np.eye(2) - Ham(k, t_vec[i])) + np.eye(2)*broad**2)
+        DOS_list[meas_count] = broad/np.pi*np.trace(delta_op, axis1=1, axis2=2)
+        N_list[meas_count] = broad/np.pi*np.trace(delta_op@rho, axis1=1, axis2=2)
+        meas_count += 1
+    # Time evolution of relevant variables
+    rho = U@rho@np.conj(U.T)
 
-# %%
-DOS_list_int = simpson(DOS_list, al_list, axis=0)
-N_list_int = simpson(N_list, al_list, axis=0)
-#DOS_list_int = DOS_list[0]
-#N_list_int = N_list[0]
 
-# %%
 cmap = plt.cm.plasma 
 norm = Normalize(vmin=t_vec[0]/T, vmax=t_vec[-1]/T)
+
 
 # DOS (Energy)
 fig, ax = plt.subplots()
 for i in range(N_measures):
-    ax.plot(EF_list, N_list_int[i,:], color=cmap(norm(t_vec_measures[i])), label=f't={round(t_vec_measures[i]/T, 3)}T')
+    ax.plot(EF_list, N_list[i,:], color=cmap(norm(t_vec_measures[i])), label=f't={round(t_vec_measures[i]/T, 3)}T')
 #ax.legend()
 cbar = fig.colorbar(ScalarMappable(norm=norm, cmap=cmap), ax=ax, orientation='vertical', label='Time (periods)')
 ax.set_xlabel('Energy (eV)')
@@ -138,23 +134,10 @@ ax.set_ylabel('$n(\\varepsilon)$')
 ax.vlines([0], 0, np.max(DOS_list), color='grey', ls='--', alpha=0.8, zorder=1)
 ax.set_title('Density of States')
 
-# %%
 fig, ax = plt.subplots()
-ax.plot(t_vec_measures/T, N_list_int[ :,19], c='blue')
-ax.plot(t_vec_measures/T, N_list_int[:,-20], c='red')
+ax.plot(t_vec_measures/T, N_list[ :,19], c='blue')
+ax.plot(t_vec_measures/T, N_list[:,-20], c='red')
 ax.set_xlabel('Tp')
 ax.set_ylabel('DOS*n')
 ax.set_xlim(0, 10)
-
-# %%
-# Fourier Stuff
-amp = np.abs(np.fft.rfft(N_list_int[:,19])[1:])
-freqs = np.fft.rfftfreq(N_measures, t_vec_measures[1])[1:]*2*np.pi
-
-fig, ax = plt.subplots()
-ax.plot(freqs/omega1, amp)
-ax.set_xlim(0, 10)
-
-
-
 # %%
